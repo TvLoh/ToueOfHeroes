bb600f57165496cf95cbeed4ceaa7247f87e3cd0
Thomas loh <fips.froelich@gmail.com>
2020-11-22 19:36:13 +0100

    initialise github

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Windstorm' },
+  { id: 12, name: 'Sandstorm' },
+  { id: 13, name: 'Icestorm' },
+  { id: 14, name: 'Starstorm' },
+  { id: 15, name: 'Trashstorm' },
+  { id: 16, name: 'Lovestorm' },
+  { id: 17, name: 'Ansturm' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
